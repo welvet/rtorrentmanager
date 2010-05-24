663fa33dff0e7368b07600114a057b2fb385c192
welvet <welvet@16c2aebc-5d17-9161-2055-b0f90ec366a7>
2010-05-24 17:38:30 +0000

    Task: Работа через jquery dataTable [welvet]

--- trunk/web/js/core.js
+++ trunk/web/js/core.js
@@ -1,21 +1,33 @@
 $(document).ready(function() {
-    $('#torrentTable').dataTable({
-        bPaginate:false,
-        bInfo:false,
-        aoColumns: [
-            null,//            <th>Имя</th>
-            {bVisible:false},//            <th>hash</th>
-            null,//            <th>Статус</th>
-            null,//            <th>&nbsp;</th>
-            null,//            <th>Скачано</th>
-            null,//            <th>Размер</th>
-            null,//            <th>Ратио</th>
-            null,//            <th>Пиры</th>
-            null//            <th>Сиды</th>
+    //инициализация таблицы рторрента
+    //todo потом эта страница будет загружаться аяксом
+    function initializeTable() {
+        $('#torrentTable').dataTable({
+            bPaginate:false,
+            bInfo:false,
+            bJQueryUI: true,
+            bLengthChange: false,
+            aoColumns: [
+                null,//            <th>Имя</th>
+                {bVisible:false},//            <th>hash</th>
+                null,//            <th>Статус</th>
+                null,//            <th>&nbsp;</th>
+                null,//            <th>Скачано</th>
+                null,//            <th>Размер</th>
+                null,//            <th>Ратио</th>
+                null,//            <th>Пиры</th>
+                null//            <th>Сиды</th>
 
-        ]
-        //        "bProcessing": true,
-        //        "bServerSide": true,
-        //        "sAjaxSource": "/?do=tbl"
-    });
+            ],
+            "bProcessing": true,
+            "sAjaxSource": "mockSource.html"
+        });
+    }
+
+    //public static void main(null)
+    initializeTable();
+    $("#dialogBody").tabs();
+    $("#dialog").dialog({ modal: false, resizable: false,
+        draggable: true, width: 800, height: 500 });
+    $.widget("#leftMenu");
 });
