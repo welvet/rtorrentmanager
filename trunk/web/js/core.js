6ad4faf9d7ad0df9b35fd9ca82c227808d529015
welvet <welvet@16c2aebc-5d17-9161-2055-b0f90ec366a7>
2010-05-25 18:59:01 +0000

    Task: Работа через jquery dataTable [welvet]

--- trunk/web/js/core.js
+++ trunk/web/js/core.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    //инициализация статических объектов
+    var selectedTorrents = [];
     //инициализация таблицы рторрента
     //todo потом эта страница будет загружаться аяксом
     function initializeTable() {
@@ -6,28 +8,59 @@ $(document).ready(function() {
             bPaginate:false,
             bInfo:false,
             bJQueryUI: true,
-            bLengthChange: false,
-            aoColumns: [
-                null,//            <th>Имя</th>
-                {bVisible:false},//            <th>hash</th>
-                null,//            <th>Статус</th>
-                null,//            <th>&nbsp;</th>
-                null,//            <th>Скачано</th>
-                null,//            <th>Размер</th>
-                null,//            <th>Ратио</th>
-                null,//            <th>Пиры</th>
-                null//            <th>Сиды</th>
-
+            aoColumns: [      //устанавливаем поля
+                {bVisible: false},
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
             ],
+            bLengthChange: false,
+            fnRowCallback: rowCallback, //задаем каллбек для контекстного меню
             "bProcessing": true,
             "sAjaxSource": "mockSource.html"
         });
     }
+    //каллбек для обработки полей
+    function rowCallback(nRow, aData, iDisplayIndex) {
+        //добавляем элемент в массив
+        selectedTorrents.push(nRow);
+        $(nRow).contextMenu({
+            menu: 'contextMenu'
+        },
+                function(action, el, pos) {
+                    //тут вероятно будет кейс на функции. функция будет вида doStart(hash);
+                    alert(action + "(" + $(el).attr("hash") + ")");
+                });
+        //устанавливаем аттрибут со значением хеша
+        $(nRow).attr("hash", aData[0]);
+        //устанавливаем выделение
+        $(nRow).mousedown(function() {
+            for (var i = 0; i <= selectedTorrents.length; i++) {
+                var sel = selectedTorrents[i];
+                $(sel).removeClass("rowSelected");
+            }
+            $(this).addClass("rowSelected");
+        });
+        var img = $(nRow).children().get(1);
+        //устанавливаем изображение для статуса торрента
+        $(img).html("<img src=\"images/"+aData[2]+".jpg\"/>");
+        return nRow;
+    }
+
+    //открыть диалог с настройками
+    function openSettingsDialog() {
+        $("#dialogBody").tabs();
+        $("#dialog").dialog({ modal: false, resizable: false,
+            draggable: true, width: 800, height: 500 });
+        $.widget("#leftMenu");
+    }
+
 
     //public static void main(null)
     initializeTable();
-    $("#dialogBody").tabs();
-    $("#dialog").dialog({ modal: false, resizable: false,
-        draggable: true, width: 800, height: 500 });
-    $.widget("#leftMenu");
 });
