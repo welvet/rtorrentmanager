161327f0e2d9d488192841f33d0f0fca7ea85e41
welvet <welvet@16c2aebc-5d17-9161-2055-b0f90ec366a7>
2010-05-29 17:49:06 +0000

    Task: Написать ант таски для сборки проекта

--- core/src/rtorrent/test/WebServerTest.java
+++ core/src/rtorrent/test/WebServerTest.java
@@ -1,12 +1,29 @@
 package rtorrent.test;
 
 import junit.framework.TestCase;
+import winstone.Launcher;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * User: welvet
  * Date: 18.05.2010
  * Time: 20:51:07
  */
-public class WebServerTest extends TestCase{
-    
+public class WebServerTest extends TestCase {
+    public void testStart() throws Exception {
+//        todo REAZIE ME
+        Map<String, String> properties = new HashMap<String, String>();
+        properties.put("accessLoggerClassName", "rtorrent.utils.WebServLogger");
+        properties.put("ajp13Port", "-1");
+        properties.put("httpPort", "8080");
+        properties.put("httpsListenAddress     ", "127.0.0.1");
+        properties.put("webroot", "C:/");
+        Launcher.initLogger(properties);
+        Launcher winstone = new Launcher(properties);
+        while (true) {
+            Thread.sleep(1000);
+        }
+    }
 }
